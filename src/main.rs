acb5886da248eec33eedd233c06a31e82bf250f0
Matt Butcher <matt.butcher@microsoft.com>
2019-08-06 22:41:17 -0600

    ran cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@
 extern crate serde_derive;
 
 use kube::{
-    api::{Object, RawApi, Informer, WatchEvent, Void},
+    api::{Informer, Object, RawApi, Void, WatchEvent},
     client::APIClient,
     config,
 };
-
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Book {
@@ -20,13 +19,13 @@ type KubeBook = Object<Book, Void>;
 fn main() {
     // Load the kubeconfig file.
     let kubeconfig = config::load_kube_config().expect("kubeconfig failed to load");
-    
+
     // Create a new client
     let client = APIClient::new(kubeconfig);
 
     // Set a namespace. We're just hard-coding for now.
     let namespace = "default";
-    
+
     // Describe the CRD we're working with.
     // This is basically the fields from our CRD definition.
     let resource = RawApi::customResource("books")
@@ -34,7 +33,9 @@ fn main() {
         .within(&namespace);
 
     // Create our informer and start listening.
-    let informer = Informer::raw(client, resource).init().expect("informer init failed");
+    let informer = Informer::raw(client, resource)
+        .init()
+        .expect("informer init failed");
     loop {
         informer.poll().expect("informer poll failed");
 
@@ -46,16 +47,13 @@ fn main() {
 }
 
 fn handle(event: WatchEvent<KubeBook>) {
-    // This will receive events each time something 
+    // This will receive events each time something
     match event {
-        WatchEvent::Added(book) => {
-            println!("Added a book {} with title '{}'", book.metadata.name, book.spec.title)
-        },
-        WatchEvent::Deleted(book) => {
-            println!("Deleted a book {}", book.metadata.name)
-        }
-        _ => {
-            println!("another event")
-        }
+        WatchEvent::Added(book) => println!(
+            "Added a book {} with title '{}'",
+            book.metadata.name, book.spec.title
+        ),
+        WatchEvent::Deleted(book) => println!("Deleted a book {}", book.metadata.name),
+        _ => println!("another event"),
     }
 }
